eabb129780ca497e20c7f255aea87703155849d0
airacalins <airacalins@gmail.com>
2022-05-04 11:05:24 +0800

    initial commit

---
+++ lib/home.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int ballNumber = 1;
+
+  void handleBallChange() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   return Container(
+      color: Colors.black,
+      child: Center(
+        child: Row(children: [
+          Expanded(
+            child: TextButton(
+              onPressed: handleBallChange,
+              child: Image.asset('assets/images/ball$ballNumber.png'),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:flutter_playground/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Magic 8 Ball'),
+          backgroundColor: Colors.red,
+        ),
+        body: Home(),
+      ),
+    );
+  }
+}
